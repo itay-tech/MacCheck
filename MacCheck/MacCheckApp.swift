c75d5f7a3cae6e339af2bb155c897b807ab203db
itayzer <itayzer2@gmail.com>
2026-05-31 21:18:56 +0300

    Initial Commit

---
+++ MacCheck/MacCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacCheckApp.swift
+//  MacCheck
+//
+//  Created by Itay Zer on 31/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct MacCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MacCheckTests/MacCheckTests.swift
@@ -0,0 +1,17 @@
+//
+//  MacCheckTests.swift
+//  MacCheckTests
+//
+//  Created by Itay Zer on 31/05/2026.
+//
+
+import Testing
+@testable import MacCheck
+
+struct MacCheckTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
